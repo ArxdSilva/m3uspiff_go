6ba5434d7b1fa646a3f9bf97110e40d6a2d8930d
ibrokemypie <ibrokemypie@bastardi.net>
2017-05-04 17:12:11 +1000

    Take one argument

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello World")
+	argcheck()
+	fmt.Println(os.Args[1])
+}
+
+func argcheck() {
+	if len(os.Args) != 2 {
+		err := errors.New("m3uspiff takes exactly ONE argument \n")
+		fmt.Print(err)
+		os.Exit(1)
+	}
 }
