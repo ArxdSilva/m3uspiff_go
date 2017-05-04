3d814b0348bc1de522091383d1e31e2bd36d1e5a
ibrokemypie <ibrokemypie@bastardi.net>
2017-05-04 18:20:52 +1000

    Split source over multiple files for readability

--- command.go
+++ command.go
@@ -6,13 +6,9 @@ import (
 )
 
 func main() {
-	argcheck()
-	fmt.Println(os.Args[1])
-}
-
-func argcheck() {
 	if len(os.Args) != 2 {
 		fmt.Println("m3uspiff takes exactly ONE argument \n")
 		os.Exit(1)
 	}
+	print()
 }
---
+++ print.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func print() {
+	fmt.Println(os.Args[1])
+}
