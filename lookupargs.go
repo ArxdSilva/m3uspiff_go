81abbc30dcd9ae3b8fc18f864adabed9ea6a63db
ibrokemypie <ibrokemypie@bastardi.net>
2017-05-05 23:41:54 +1000

    start reading meta with github.com/dhowden/tag

--- lookupargs.go
+++ lookupargs.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"github.com/dhowden/tag"
+	"log"
+	"os"
+)
+
 func lookupargs(line string) {
-	println(line)
+	//println(line)
+	file, err := os.Open(line)
+	if err != nil {
+		//log.Print(err)
+		return
+	}
+	tags, err := tag.ReadFrom(file)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	println(tags.Title())
+
 }
